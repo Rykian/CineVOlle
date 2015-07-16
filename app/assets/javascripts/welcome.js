e523a9d4e17f51d6b03fd0bc8464df5b1e87ea5d
Rykian <rykian@gmail.com>
2015-07-16 11:46:15 +0200

    closes #1: Confirmation is displayed in Ajax
    And moving basic layout from index page to application layout

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,14 +2,27 @@
 // All this logic will automatically be available in application.js.
 
 $(function() {
+  function displayResult(data) {
+    var name = Object.keys(data)[0];
+    var message = data[name];
+    if(name == 'error') {
+      name = 'danger';
+    }
+
+    var alert = $('<div class="alert alert-' + name + ' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + message + '</div>');
+
+    $('#header').after(alert.hide());
+    alert.slideDown();
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
 
     $.post(event.target.action,
            $(event.target).serialize(),
-           function(data) { console.log(data) },
+           displayResult,
            'json')
   }
 
-  $('form').on('submit', handleSubmit )
+  $('form').on('submit', handleSubmit );
 })
